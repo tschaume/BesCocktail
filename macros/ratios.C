09ff2681c01373dbf7287226ea343eeda4fde159
Patrick Huck <phuck@lbl.gov>
2013-04-18 19:20:18 -0700

    update ratios.C

--- macros/ratios.C
+++ macros/ratios.C
@@ -17,10 +17,10 @@ void ratios(bool qm11 = true) {
   TString hnMe[np]  = {
     "pion", "eta", "etap", "omega", "phi", "jpsi", "ccbar", "hCocktail"
   };
-  double hrX1[np] = {0, 0, 0, 0, 0, 2.5, 0, 0};
-  double hrX2[np] = {0.12, 0.6, 1.1, 0.9, 1.2, 3.2, 2.8, 3.3};
-  double hrY1[np] = {0.5, 0.37, 0.45, 0.4, 0.3, 0.1, 0.8, 0.4};
-  double hrY2[np] = {0.8, 0.7, 0.8, 0.8, 14, 1, 1.4, 1.1};
+  double hrX1[np] = {0.00, 0.00, 0.00, 0.00, 0.00, 2.70, 0.0, 0.0};
+  double hrX2[np] = {0.12, 0.55, 0.90, 0.90, 1.15, 3.20, 2.8, 3.3};
+  double hrY1[np] = {0.80, 0.45, 0.70, 0.70, 0.70, 0.85, 0.8, 0.4};
+  double hrY2[np] = {1.10, 1.05, 1.05, 1.05, 30.0, 1.10, 1.2, 1.1};
 
   // cocktail
   TCanvas* cc = (TCanvas*)fMe->Get("cCocktail");
@@ -28,6 +28,8 @@ void ratios(bool qm11 = true) {
   TH1D* ht;
   if ( qm11 ) ht = (TH1D*)fXin->Get(hnXin[7].Data());
   else ht = (TH1D*)fJie->Get(hnJie[7].Data());
+  gPad->SetLogy();
+  gPad->SetGrid();
   ht->Draw("hsame");
 
   // ratios
@@ -38,6 +40,7 @@ void ratios(bool qm11 = true) {
   for ( int i = 0; i < np; ++i ) {
     c->cd(i+1);
     if ( i == 4 ) gPad->SetLogy();
+    gPad->SetGrid();
     hR[i] = (TH1D*)fMe->Get(hnMe[i].Data());
     if ( qm11 ) {
       TH1D* h = (TH1D*)fXin->Get(hnXin[i].Data());
@@ -48,7 +51,7 @@ void ratios(bool qm11 = true) {
     hR[i]->GetXaxis()->SetRangeUser(hrX1[i], hrX2[i]);
     hR[i]->GetYaxis()->SetRangeUser(hrY1[i], hrY2[i]);
     hR[i]->Draw();
-    hR[i]->GetYaxis()->UnZoom();
+    //hR[i]->GetYaxis()->UnZoom();
   }
 
   c->Print("CocktailRatio200GeV.png");
