735eddaec7c94593245286496d2eb1a59807fb05
Patrick Huck <phuck@lbl.gov>
2014-05-09 18:03:17 -0700

    getAllCocktail.sh: update LMR/omega regions

--- macros/getAllCocktail.sh
+++ macros/getAllCocktail.sh
@@ -7,8 +7,8 @@ for i in 19 27 39 62; do
   root -l -b -x -q getCocktail.C\($i,\"hCocktail\",\"cocktail\",\"\"\)
   root -l -b -x -q getCocktail.C\($i,\"hCocktailPt_IMR\",\"cocktail\",\"_IMR_1.2-2.5\"\)
   root -l -b -x -q getCocktail.C\($i,\"hCocktailPt_jpsi\",\"cocktail\",\"_jpsi_2.97-3.22\"\)
-  root -l -b -x -q getCocktail.C\($i,\"hCocktailPt_LMR\",\"cocktail\",\"_LMR_0.2-0.6\"\)
-  root -l -b -x -q getCocktail.C\($i,\"hCocktailPt_omega\",\"cocktail\",\"_omega_0.75-0.82\"\)
+  root -l -b -x -q getCocktail.C\($i,\"hCocktailPt_LMR\",\"cocktail\",\"_LMR_0.3-0.76\"\)
+  root -l -b -x -q getCocktail.C\($i,\"hCocktailPt_omega\",\"cocktail\",\"_omega_0.76-0.82\"\)
   root -l -b -x -q getCocktail.C\($i,\"hCocktailPt_phi\",\"cocktail\",\"_phi_0.98-1.06\"\)
   root -l -b -x -q getCocktail.C\($i,\"hCocktailPt_pi0\",\"cocktail\",\"_pi0_0-0.2\"\)
 done
